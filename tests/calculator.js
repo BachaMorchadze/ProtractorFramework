bbd9ff58d64cd7de0883781948c063c5cd3d192d
BachaMorchadze <bachamorchadze@gmail.com>
2021-02-05 12:15:01 +0400

    commented substraction method

--- tests/calculator.js
+++ tests/calculator.js
@@ -25,28 +25,28 @@ describe('demo calculator test', function () {
 
     });
 
-    it('substraction test', function () {
+    // it('substraction test', function () {
 
-        // browser.get('https://juliemr.github.io/protractor-demo/');
-        homepage.get('https://juliemr.github.io/protractor-demo/');
+    //     // browser.get('https://juliemr.github.io/protractor-demo/');
+    //     homepage.get('https://juliemr.github.io/protractor-demo/');
 
-        // element(by.model('first')).sendKeys('2');
-        homepage.enterFirstNumber('2');
+    //     // element(by.model('first')).sendKeys('2');
+    //     homepage.enterFirstNumber('2');
 
-        // element(by.model('second')).sendKeys('3');
-        homepage.enterSecondNumber('3');
+    //     // element(by.model('second')).sendKeys('3');
+    //     homepage.enterSecondNumber('3');
 
-        // element(by.css('[ng-click="doAddition()"]')).click();
-        homepage.clickGo1();
+    //     // element(by.css('[ng-click="doAddition()"]')).click();
+    //     homepage.clickGo1();
 
-        // let result = element(by.cssContainingText('.ng-binding','5'));
-        // expect(result.getText()).toEqual('5');
-        homepage.verifyResult('5');
+    //     // let result = element(by.cssContainingText('.ng-binding','5'));
+    //     // expect(result.getText()).toEqual('5');
+    //     homepage.verifyResult('5');
 
-        browser.sleep(2000);
+    //     browser.sleep(2000);
 
 
-    });
+    // });
 
 
 });
